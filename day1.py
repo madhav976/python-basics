c324758108e27e0940d5d60c36719710ce6a1980
Madhav Saini <madhavsaini.chd@gmail.com>
2026-05-18 12:47:15 +0530

    Add frequency counting function for words in string

---
+++ day1.py
@@ -0,0 +1,10 @@
+string = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go"
+def freq(x,string):
+    words = []
+    words = string.split()
+    dict= {}
+    for key in words :
+        if key == x :
+            dict[x] = words.count(x)
+    return dict
+print(freq("little", string))
